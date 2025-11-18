e0d0a24e7319e8db105a5061beb2a7277463e21a
Lê Ngọc Khang Duy <duydev1812@gmail.com>
2025-11-18 21:54:31 +0700

    Update Customer.java

--- src/main/java/com/yourteamname/store/model/Customer.java
+++ src/main/java/com/yourteamname/store/model/Customer.java
@@ -1,5 +1,61 @@
 package com.yourteamname.store.model;
 
 public class Customer {
-    
+    private String nameCus, CSN, phoneNum, emailCus, addCus;
+
+    public Customer(String nameCus, String CSN, String phoneNum, String emailCus, String addCus){
+        this.nameCus = nameCus;
+        this.CSN = CSN;
+        this.phoneNum = phoneNum;
+        this.emailCus = emailCus;
+        this.addCus = addCus;
+    }
+
+    final public String getNameCus(){
+        return this.nameCus;
+    }
+
+    final public String getCSN(){
+        return this.CSN;
+    }
+
+    final public String getPhoneNum(){
+        return this.phoneNum;
+    }
+
+    final public String getEmailCus(){
+        return this.emailCus;
+    }
+
+    final public String getAddCus(){
+        return this.addCus;
+    }
+
+    public void setNameCus(String nameCus){
+        this.nameCus = nameCus;
+    }
+
+    public void setCSN(String CSN){
+        this.CSN = CSN;
+    }
+
+    public void setPhoneNum(String phoneNum){
+        this.phoneNum = phoneNum;
+    }
+
+    public void setEmailCus(String emailCus){
+        this.emailCus = emailCus;
+    }
+
+    public void setAddCus(String addCus){
+        this.addCus = addCus;
+    }
+
+    public String getDescription(){
+    return "Name of Customer: " + getNameCus() + 
+        "\n CSN: " + getCSN() +
+        "\n Phone number: " + getPhoneNum() +
+        "\n Email: " + getEmailCus() +
+        "\n Address: " + getAddCus();
+    }
 }
